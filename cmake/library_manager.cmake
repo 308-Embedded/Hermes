ab2d558d101572ae725c231e0652f9b130ad80d3
geniusdo <2403644992@qq.com>
2025-10-23 12:24:04 +0800

    try compile libraries

--- cmake/library_manager.cmake
+++ cmake/library_manager.cmake
@@ -6,9 +6,10 @@ function(DECLARE_LIB LIB_NAME UUID HEADER SOURCE_FILE COMPILE_OPTION LINK_OPTION
     if(index GREATER -1)
         message(FATAL_ERROR "Library UUID '${UUID}' already exists!")
     else()
-        list(APPEND ${GLOBAL_LIBRARIES} "${UUID}")
+        list(APPEND GLOBAL_LIBRARIES "${UUID}")
+        set(GLOBAL_LIBRARIES "${GLOBAL_LIBRARIES}" CACHE STRING "global libraries" FORCE)
     endif()
-    
+
     set(${UUID}_LIB_NAME "${LIB_NAME}" CACHE STRING "Library name for ${UUID}" FORCE)
     set(${UUID}_HEADER "${HEADER}" CACHE STRING "Header file for ${UUID}" FORCE)
     set(${UUID}_SOURCE_FILE "${SOURCE_FILE}" CACHE STRING "Source file for ${UUID}" FORCE)
@@ -28,6 +29,20 @@ function(DECLARE_LIB LIB_NAME UUID HEADER SOURCE_FILE COMPILE_OPTION LINK_OPTION
         set(${LIB_NAME}_UUIDS "${${LIB_NAME}_UUIDS}" CACHE STRING "Mapping of UUIDs for ${LIB_NAME}" FORCE)
     endif()
 
-    message(STATUS "Library ${LIB_NAME} created with UUID: ${UUID} \nSources: ${SOURCE_FILE}")
+    message(STATUS "Library ${LIB_NAME} created with UUID: ${UUID}")
+
+endfunction()
+
+# compile libraries listed in GLOBAL_LIBRARIES
+function(COMPILE_ALL_LIBS)
+
+    foreach(UUID ${GLOBAL_LIBRARIES})
+        add_library(${UUID} STATIC ${${UUID}_SOURCE_FILE})
+        set_target_properties(${UUID} PROPERTIES OUTPUT_NAME "${${UUID}_LIB_NAME}")
+        target_include_directories(${UUID} PRIVATE ${${UUID}_HEADER})
+        target_compile_options(${UUID} PRIVATE ${${UUID}_COMPILE_OPTION})
+        target_link_libraries(${UUID} PRIVATE ${${UUID}_LINK_OPTION})
+        message(STATUS "Compiling library: ${${UUID}_LIB_NAME}")
+    endforeach()
 
 endfunction()
